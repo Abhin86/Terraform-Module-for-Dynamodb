4ddd650e07e8744dd5e3009aae60e9815b5fc59c
root <root@Urolime>
2024-05-28 15:04:28 +0530

    awsn

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 # Configure the AWS Provider
-provider "aws" {
-  profile = "default"
-  region  = var.aws_region
+# provider "aws" {
+#   profile = "default"
+#   region  = var.aws_region
   
-}
+# }
 
