c4457dd64d1b33f23f133094af3cc70b8cdd91b2
root <root@Urolime>
2024-05-29 10:39:50 +0530

    lihbkhj

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Define the AWS provider
 provider "aws" {
   region = var.aws_region
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  # access_key = var.AWS_ACCESS_KEY
+  # secret_key = var.AWS_SECRET_KEY
 }
