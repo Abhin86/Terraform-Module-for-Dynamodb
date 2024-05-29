05717a7867940cd971a89e482418bcf93bc8b5c7
root <root@Urolime>
2024-05-29 10:08:53 +0530

    module

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 module "dynamodb" {
     source = "./modules/dynamodb"
+  name     = "my-table"
+  hash_key = "id"
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
 }
