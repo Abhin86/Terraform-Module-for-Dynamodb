3337e8d885bc7a29441b0d001bf61aaea1485a68
abhin <abhin.kumar@urolime.com>
2024-05-30 13:53:21 +0530

    fmt

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "dynamodb" {
-  source    = "./modules/dynamodb"
-  name      = "my-table"
-  hash_key  = "BrandName"
-  range_key = "ModelNumber"
+  source                         = "./modules/dynamodb"
+  name                           = "my-table"
+  hash_key                       = "BrandName"
+  range_key                      = "ModelNumber"
   point_in_time_recovery_enabled = true
   server_side_encryption_enabled = false
   attributes = [
